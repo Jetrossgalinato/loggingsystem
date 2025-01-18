0147ddfbfdb6aa59eee4809e1ad015ecc246e4aa
Jetross <jetrossneri07@gmail.com>
2025-01-18 08:55:22 +0800

    feat: added prettier

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php session_start();?>
+<?php session_start(); ?>
 <html>
     <head>
         <script type="text/javascript" src="js/adapter.min.js"></script>
@@ -8,80 +8,96 @@
     </head>
 
     <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <video style="margin-top: 50px" id="preview" width="100%"></video>
-                <?php
-                    if(isset($_SESSION['error'])){
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <video style="margin-top: 50px" id="preview" width="100%"></video>
+                    <?php
+                    if (isset($_SESSION['error'])) {
                         echo "<div class='alert alert-danger' id='error-message'>
-                        <h4>Error!</h4>
-                        ".$_SESSION['error']."
-                        </div>";
+                            <h4>Error!</h4>
+                            " .
+                            $_SESSION['error'] .
+                            "
+                            </div>";
                         unset($_SESSION['error']);
                     }
 
-                    if(isset($_SESSION['success'])){
+                    if (isset($_SESSION['success'])) {
                         echo "<div class='alert alert-success' id='success-message'>
-                        <h4>Success!</h4>
-                        ".$_SESSION['success']."
-                        </div>";
+                            <h4>Success!</h4>
+                            " .
+                            $_SESSION['success'] .
+                            "
+                            </div>";
                         unset($_SESSION['success']);
                     }
-                ?>
-            </div>
-            <div class="col-md-6">
-                <form action="insert.php" method="post" class="form-horizontal">
-                    <label style="margin-top: 50px;">SCAN QR CODE</label>
-                    <input type="text" name="text" id="text" readonly="" class="form-control">
-                </form>
-                <form method="GET" action="" class="form-inline" style="margin-bottom: 20px;">
-                    <input type="text" name="search" class="form-control" placeholder="Search Student ID" 
-                    value="<?php echo isset($_GET['search']) ? $_GET['search'] : ''; ?>">
-                    <button type="submit" class="btn btn-primary" style="margin-left: 10px;">Search</button>
-                </form>
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <td>Student ID</td>
-                            <td>Log Time</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                            $server = "localhost";
-                            $username = "root";
-                            $password = "";
-                            $database = "qrcodedb";
-                        
-                            $conn = new mysqli($server, $username, $password, $database);
-                        
+                    ?>
+                </div>
+                <div class="col-md-6">
+                    <form action="insert.php" method="post" class="form-horizontal">
+                        <label style="margin-top: 50px;">SCAN QR CODE</label>
+                        <input type="text" name="text" id="text" readonly="" class="form-control">
+                    </form>
+                    <form method="GET" action="" class="form-inline" style="margin-bottom: 20px;">
+                        <input type="text" name="search" class="form-control" placeholder="Search Student ID" 
+                        value="<?php echo isset($_GET['search'])
+                            ? $_GET['search']
+                            : ''; ?>">
+                        <button type="submit" class="btn btn-primary" style="margin-left: 10px;">Search</button>
+                    </form>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <td>Student ID</td>
+                                <td>Log Time</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $server = 'localhost';
+                            $username = 'root';
+                            $password = '';
+                            $database = 'qrcodedb';
+
+                            $conn = new mysqli(
+                                $server,
+                                $username,
+                                $password,
+                                $database
+                            );
+
                             if ($conn->connect_error) {
-                                die("Connection failed: " . $conn->connect_error);
-                            } 
-                                $search = isset($_GET['search']) ? $conn->real_escape_string($_GET['search']) : '';
-                                $sql = "SELECT student_id, time_in FROM table_attendance";
-                                
-                                if (!empty($search)) {
-                                    $sql .= " WHERE student_id LIKE '%$search%'";
-                                }
-                                
-                                $query = $conn->query($sql);
-                                
-                                while($row = $query->fetch_assoc()){     
-                        ?>
-                                <tr>
-                                    <td><?php echo $row['student_id']; ?></td>
-                                    <td><?php echo $row['time_in']; ?></td>
-                                </tr>
-                        <?php
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                                die(
+                                    'Connection failed: ' . $conn->connect_error
+                                );
+                            }
+                            $search = isset($_GET['search'])
+                                ? $conn->real_escape_string($_GET['search'])
+                                : '';
+                            $sql =
+                                'SELECT student_id, time_in FROM table_attendance';
+
+                            if (!empty($search)) {
+                                $sql .= " WHERE student_id LIKE '%$search%'";
+                            }
+
+                            $query = $conn->query($sql);
+
+                            while ($row = $query->fetch_assoc()) { ?>
+                                    <tr>
+                                        <td><?php echo $row[
+                                            'student_id'
+                                        ]; ?></td>
+                                        <td><?php echo $row['time_in']; ?></td>
+                                    </tr>
+                            <?php }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
         <script>
             let Scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
             Instascan.Camera.getCameras().then(function (cameras) {
--- insert.php
+++ insert.php
@@ -1,25 +1,27 @@
  <?php
-    session_start();
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "qrcodedb";
- 
-    $conn = new mysqli($server, $username, $password, $database);
+ session_start();
+ $server = 'localhost';
+ $username = 'root';
+ $password = '';
+ $database = 'qrcodedb';
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
+ $conn = new mysqli($server, $username, $password, $database);
 
-    if(isset($_POST['text'])){
-        $text = $_POST['text'];
-        $sql = "INSERT INTO table_attendance (student_id,time_in) VALUES ('$text',now())";
-        if ($conn->query($sql) === TRUE) {
-            $_SESSION['success'] = "New record created successfully";
-        } else {
-            $_SESSION['error'] = $conn->error;
-        }
-        header("Location: index.php");
-    }
-    $conn->close();
- ?>
+ if ($conn->connect_error) {
+     die('Connection failed: ' . $conn->connect_error);
+ }
+
+ if (isset($_POST['text'])) {
+     $text = $_POST['text'];
+     $sql = "INSERT INTO table_attendance (student_id,time_in) VALUES ('$text',now())";
+     if ($conn->query($sql) === true) {
+         $_SESSION['success'] = 'New record created successfully';
+     } else {
+         $_SESSION['error'] = $conn->error;
+     }
+     header('Location: index.php');
+ }
+ $conn->close();
+
+
+?>
